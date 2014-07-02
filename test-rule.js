61eeb5e0ac47b9b2cf92398fc30dc91f2d2dd14d
efa2000 <efa2000@mail.ru>
2014-07-02 17:24:45 +0400

    example

--- test-rule.js
+++ test-rule.js
@@ -1,19 +1,11 @@
-var fire = require('./rule-machine.js');
-
-var rule = new fire({
-    onlyNewFire : true
+var rm = require('./rule-machine.js');
+var rule = new rm({
+    onlyNewFire : true // Fired only new events (default: true)
 });
 
-
-rule.on('FIRE',function(fired){
+rule.on('FIRE',function(fired){ //Names array of fired rules
     console.log(fired);
 });
-
-rule.addFact({'id':1,'val':10}, false);
-rule.addFact({'id':2,'val':20}, false);
-rule.addFact({'id':3,'val':30}, false);
-rule.addFact({'id':4,'val':40}, false);
-rule.addFact({'id':5,'val':50}, false);
 
 f = [
     {'id':6,'val':60},
@@ -21,61 +13,41 @@ f = [
     {'id':8,'val':80}
 ];
 
-rule.addFact(f);
-
-console.log(rule.getFact());
+rule.addFact(f, false); // Add array of facts. And NOT check rules after add
 
 rul =[ 
     {
         name: 'Test1',
         when:[
             'and',
-            {id:1, val:{$gt:10}},
+            {id:1, val:{$gte:10}},
             {id:3, val:30}
         ]
     },{
         name: 'Test2',
         when:[
             'and',
-            {id:2, val:{$gt:10}}
+            {id:2, val:{$gt:20}}
         ]
     },{
         name: 'Test3',
         when:[
-            'and',
-            {id:2, val:{$gt:10}},
-            {id:5, lastVal: {$gt:50} }
+            'or',
+            {id:4, val:{$lt:10}},
+            {id:5, lastVal: {$gte:50} }
         ]
     }
 ];
 
-rule.addRule(rul);
-//console.log(rule.getRule());
-//console.log(rule.getFact());
+rule.addRule(rul); // Add array of rules
 
-//rule.check();
-//console.log(rule.getRule());
+// add other facts
+rule.addFact({'id':1,'val':10}, true); // add fact and check rules after add
+rule.addFact({'id':2,'val':2});       // it same 
+rule.addFact({'id':3,'val':30});
+rule.addFact({'id':4,'val':40});
+rule.addFact({'id':5,'val':50}, false); // add fact and NOT check rules after add
 
-//rule.modFact({'lastVal':30}, {'val':{$lte:30}});
-rule.modFact({'val':125}, {'id':1});
+rule.modFact({'val':125}, {'id':2}); // modify fact where id=2 and set val=125
 
-//rule.modFact({'lastVal':30}, {'val':{$lte:30}});
-console.log(rule.getFact());
-rule.modFact({'val':1}, {'val':{$gt:1}})
-
-//rule.delRule();
-
-//rule.modFact({'val':10}, {'id':1});
-console.log(rule.getFact());
-
-rule.modFact({'val':100}, {'val':1})
-
-rule.modFact({'lastVal':60}, {'id':5});
-
-//console.log(rule.getFact());
-
-rule.modFact({'val':15}, {'id':1});
-rule.modFact({'val':30}, {'id':3});
-
-
-
+rule.modFact({'lastVal':60}, {'id':5}); //modify fact where id=5 and add new property lastVal=60
